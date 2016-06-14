7d7b0fd0c75b86327019b868f0c63d7a9830b638
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-14 10:44:41 -0400

    has one bug on too many keyups

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
   var songNum=1;
-  var audio = new Audio("http://www.lynnemusic.com/samples/spirit-voices-quick-demo.mp3");
+  var audio = new Audio("http://www.nasa.gov/mp3/640392main_STS-26_Liftoff.mp3");
  $( document ).ready(function() {
-    console.log( "ready!" );
+    updateTime();
+    $("#songNum").html('<h1>'+ "playing song 1 of 3"+'</h1>');
 
 });
     function input()
@@ -14,10 +15,12 @@
         if(command== "play") play();
         if(command== "next") next();
         if(command== "prev") prev();
+        return false;
     }
     function stop()
     {
         audio.pause();
+        // stop resets the song, it doesn't pause it
         audio.load();
     }
     function play()
@@ -31,24 +34,27 @@
           audio.pause();
           audio = new Audio("http://www.lynnemusic.com/samples/spirit-voices-quick-demo.mp3");
           songNum=2;
-          audio.play
-          console.log("was 1");
+          audio.load();
+          audio.play();
+          $("#songNum").html('<h1>'+ "playing song 2 of 3"+'</h1>');
         }
         else if (songNum==2)
         {
           audio.pause();
           audio = new Audio("http://www.sounds.beachware.com/2illionzayp3may/joptlz/CHIMES.mp3");
           songNum=3;
-          audio.play
-          console.log("was 2");
+          audio.load();
+          audio.play();
+           $("#songNum").html('<h1>'+ "playing song 3 of 3"+'</h1>');
         }
         else if(songNum==3)
         {
           audio.pause();
           audio = new Audio("http://www.nasa.gov/mp3/640392main_STS-26_Liftoff.mp3");
           songNum=1;
-          audio.play
-          console.log("was 3");
+          audio.load();
+          audio.play();
+           $("#songNum").html('<h1>'+ "playing song 1 of 3"+'</h1>');
         }
     }
     function prev()
@@ -56,23 +62,29 @@
         if (songNum==1)
         {
           audio.pause();
-          audio = new Audio("http://www.nasa.gov/mp3/640392main_STS-26_Liftoff.mp3");
+          audio = new Audio("http://www.sounds.beachware.com/2illionzayp3may/joptlz/CHIMES.mp3");
           songNum=3;
-          audio.play
+          audio.load();
+          audio.play();
+          $("#songNum").html('<h1>'+ "playing song 3 of 3"+'</h1>');
         }
         if (songNum==2)
         {
           audio.pause();
-          audio = new Audio("http://www.lynnemusic.com/samples/spirit-voices-quick-demo.mp3");
+          audio = new Audio("http://www.nasa.gov/mp3/640392main_STS-26_Liftoff.mp3");
           songNum=1;
-          audio.play
+          audio.load();
+          audio.play();
+          $("#songNum").html('<h1>'+ "playing song 1 of 3"+'</h1>');
         }
         if(songNum==3)
         {
           audio.pause();
-          audio = new Audio("http://www.sounds.beachware.com/2illionzayp3may/joptlz/CHIMES.mp3");
+          audio = new Audio("http://www.lynnemusic.com/samples/spirit-voices-quick-demo.mp3");
           songNum=2;
-          audio.play
+          audio.load();
+          audio.play();
+          $("#songNum").html('<h1>'+ "playing song 2 of 3"+'</h1>');
         }
     }
 
